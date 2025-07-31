fbc1b001899741adbe1889d7ff9718d9518287d2
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-07-31 00:06:23 -0600

    chore: add Vite configuration file for project setup

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	base: "/wordle/", // Example: if your app is served from example.com/my-app/
+	// Other Vite configuration options...
+});
