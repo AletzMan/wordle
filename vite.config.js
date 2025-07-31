79b5f20c27e964da3af23461af63cabaa8123483
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-07-31 00:06:19 -0600

    chore: remove Vite configuration file as it's no longer needed

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-
-export default defineConfig({
-	base: "/wordle/", // Example: if your app is served from example.com/my-app/
-	// Other Vite configuration options...
-});
